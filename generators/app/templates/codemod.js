b7e3cfc9ad6afc4bb274b59289bf3ed52e1a3e0a
James Talmage <james@talmage.io>
2016-04-17 01:40:00 -0400

    include registerMethods examples in generated output.
    It's a bit more verbose, but provides some helpful reminders about the API.

--- generators/app/templates/codemod.js
+++ generators/app/templates/codemod.js
@@ -2,19 +2,33 @@
 
 module.exports = function <%= camelName %>(file, api) {
 	var j = api.jscodeshift;
-	var ast = j(file.source);
 
-	ast.find(j.CallExpression, {
-		callee: {
-			type: 'Identifier',
-			name: 'foo'
+	// Adding a method to all collections
+	j.registerMethods({
+		findCalls: function(name) {
+			return this.find(j.CallExpression, {
+				callee: {
+					type: 'Identifier',
+					name: name
+				}
+			});
 		}
-	}).forEach(function (p) {
-		p.get('callee').replace(j.identifier('bar'));
 	});
 
-	return ast.toSource({
-		useTabs: true,
-		quote: 'single'
-	});
+	// Adding a method to all CallExpressions
+	j.registerMethods({
+		renameCallee: function (name) {
+			return this.forEach(function (path) {
+				path.get('callee').replace(j.identifier(name))
+			});
+		}
+	}, j.CallExpression);
+
+	return j(file.source)
+		.findCalls('foo')
+		.renameCallee('bar')
+		.toSource({
+			useTabs: true,
+			quote: 'single'
+		});
 };
